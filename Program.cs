0f8a6a809abe01292d1974f2fb679be03f74be23
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-11-08 08:45:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppzv5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi recenicu: ");
+
+            string[] x = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(x[0]);
+            Console.WriteLine(x[x.Length - 1]);
+
+            Console.ReadKey();
+        }
+    }
+}
